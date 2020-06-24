bc94c8be6ddfcf9d621e6ed9957f4df95e0f6278
vishakahegde <hegde.vishaka@gmail.com>
2020-06-24 11:09:23 +0200

    Add users to seeder file and create seeder file and routes for dog details

--- routers/dog.js
+++ routers/dog.js
@@ -6,9 +6,25 @@ const router = new Router();
 router.get("/", async (req, res, next) => {
   try {
     const dogs = await Dog.findAll();
-    console.log("Dog:", dogs);
     res.status(200).send(dogs);
     // res.json(dogs);
+  } catch (error) {
+    console.error(error);
+    return res.status(400).send({ message: "Something went wrong, sorry" });
+  }
+});
+
+router.get("/:id", async (req, res, next) => {
+  try {
+    const dogDetailId = req.params.id;
+    if (isNaN(parseInt(dogDetailId))) {
+      res.status(400).send("Dog Detail ID is not a number");
+    }
+    const dogDetail = await Dog.findByPk(dogDetailId);
+    if (dogDetail === null) {
+      res.status(404).send("Dog Detail not found");
+    }
+    res.status(200).send(dogDetail);
   } catch (error) {
     console.error(error);
     return res.status(400).send({ message: "Something went wrong, sorry" });
--- seeders/20200621113402-dogs.js
+++ seeders/20200621113402-dogs.js
@@ -14,6 +14,7 @@ module.exports = {
             "My names Hope! I'm a bundle of fun who's ready to find my new family.I think a house with just grown ups would be best for me, so I can keep learning lots of new things from you as I grow up.I think a house with just grown ups would be best for me, so I can keep learning lots of new things from you as I grow up.I'm a super smart and active girl and I love learning cool new skills. Taking me for lots of walks and working on some fun training will really help me gain confidence with the world.If you have a Hope-sized whole in your heart, please come in to meet me!",
           breed: "Kelpie / Staffy",
           gender: "Female",
+
           createdAt: new Date(),
           updatedAt: new Date(),
         },
@@ -26,6 +27,7 @@ module.exports = {
             "When it Rain's - it pours! I know I look like a big cuddly bear and you're half right. Although I do love a good calm snuggle from time to time I'm also looking for a home that is going to continue my training. I really enjoy tumbling around with other dogs and get so wound up when I see them that I just can't contain myself! I need to learn to control my approach when meeting other canine friends.",
           breed: "German Shepherd",
           gender: "Male",
+
           createdAt: new Date(),
           updatedAt: new Date(),
         },
@@ -38,6 +40,7 @@ module.exports = {
             "Hi I am Buff! I am a very big boy looking for a very big heart to welcome me into their home. I may weigh a tonne, but I am a lovable, gentle giant who is suitable to live with young kids, dogs and maybe even a cat if you think yours would like to play with me!It is very important that my new owners are willing to provide me with joint supplements for the rest of my life. Being such a big dog, my hips aren't in the best shape. The good news is that this is very manageable with appropriate exercise, weight management and a good quality diet. Unfortunately this means I cannot go to a home that has a staircase.",
           breed: "American Bulldog",
           gender: "Male",
+
           createdAt: new Date(),
           updatedAt: new Date(),
         },
@@ -50,6 +53,7 @@ module.exports = {
             "Hello, I'm Dalia, and I'm just as sweet as I sound. I would love a quiet home with just adults where I can be the only dog for you to shower with love. New places and people take me a little time to get used to so I'll need to take new things slowly, I'd love a family that's ready to take the extra time to continue helping me build my confidence. I might be a little shy at first but once I get to know you we will be best friends for life. I really love to run around with my friends and toys are my favourite thing in the whole world. If you have a quiet home with plenty of patience and love to give (and plenty of toys too) and you think I might be the dog for you.",
           breed: "Jack Russell Terrier",
           gender: "Female",
+
           createdAt: new Date(),
           updatedAt: new Date(),
         },
@@ -62,6 +66,18 @@ module.exports = {
             "Hey I'm Snow. I am a very shy girl who is looking for a home where everyone is above the age of 16. I would like to be the only dog in the home.I need very slow exposure to the world, as I have missed out on some very important socialisation when I was younger.I will need my humans to take it slow, be very gentle and understanding with me. I would love to go to some training too, so humans that can commit to that would be ideal!",
           breed: "Bull Arab",
           gender: "Female",
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          name: "Jethro",
+          age: "11 months",
+          imageUrl:
+            "https://www.houdenvanhonden.nl/globalassets/rassen/fci-5/alaska-malamute/ala-ou-0010.jpg",
+          aboutMe:
+            "Hey I'm Jethro. I am a young, playful and bouncy dog, who will need daily exercise, enrichment and training. True to my breed, I can be very aloof, so I would prefer go to an adults only home, as I am pretty worried about people over-handling me.I'd like to be the only dog in the home, while I very slowly work on my confidence around them. It would be best for my new parents to work with a trainer to help socialise me and assist with force-free training techniques.I will need secure fencing and a large yard, plus a home where I am allowed to spend time indoors.",
+          breed: "Alaskan Malamute",
+          gender: "Male",
           createdAt: new Date(),
           updatedAt: new Date(),
         },
