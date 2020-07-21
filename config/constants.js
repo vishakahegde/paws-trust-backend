91a7cf9e1084aa3d80b35380573c5f643f727b07
Vishaka Hegde <hegde.vishaka@gmail.com>
2020-07-21 11:27:23 +0200

    Update constants.js
    Deleted old API Key

--- config/constants.js
+++ config/constants.js
@@ -2,6 +2,6 @@ module.exports = {
   SALT_ROUNDS: 10,
   PORT: process.env.PORT || 4000,
   SENDGRID_APIKEY:
-    "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",
+    
   FROM_EMAIL: "pawstrustadoption@gmail.com",
 };
